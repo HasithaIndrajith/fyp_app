4740589a4c773a50c44770a29a653e4c7ceeec1d
root <root@DESKTOP-6Q9G1SS>
2024-04-27 15:35:50 +0530

    krenseq

--- view.py
+++ view.py
@@ -10,16 +10,30 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/seq2vec_version', methods=['GET'])
+@app.route('/seq2vec')
 def get_seq2vec_version():
     try:
         # Get seq2vec version
-        seq2vec_version = subprocess.check_output(['seq2vec', '--version']).decode().strip()
-        return jsonify({"seq2vec_version": seq2vec_version})
+        result = subprocess.run(['seq2vec', '--help'], capture_output=True, text=True)
+        output = result.stdout
+        return jsonify({"output": output})
     except subprocess.CalledProcessError as e:
         return jsonify({"error": str(e)}), 500
 
+@app.route('/kraken')
+def get_kraken_version():
+    try:
+        working_directory = '/root/kraken'
+        result = subprocess.run(['./kraken2'], capture_output=True, text=True, cwd=working_directory)
+        if result.returncode == 0:
+            output = result.stdout
+            return jsonify({"output": output})
+        else:
+            error_message = result.stderr
+            return jsonify({"error": error_message}), 500
+    except subprocess.CalledProcessError as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(debug=True, host='0.0.0.0', port=port)
